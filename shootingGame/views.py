896e642d2c3a2143d15ac42eb764d680c28dc676
TankerDragon <87114169+TankerDragon@users.noreply.github.com>
2022-05-06 01:40:34 +0500

    Update views.py

--- shootingGame/views.py
+++ shootingGame/views.py
@@ -12,12 +12,17 @@ def main(request):
 @api_view(['GET', 'POST'])
 def update(request):
     if request.method == 'GET':
-        print(get_num())
+        # print(get_num())
         return Response({"num": get_num()})
     elif request.method == 'POST':
-        print (request.data)
-        stop_looping()
-        return Response(status=status.HTTP_200_OK)
+        print(request.data)
+        if request.data['game'] == 'start':
+            start_looping()
+            return Response(status=status.HTTP_200_OK)
 
+        elif request.data['game'] == 'stop':
+            stop_looping()
+            return Response(status=status.HTTP_200_OK)
 
-
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
